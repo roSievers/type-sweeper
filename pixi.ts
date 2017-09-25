f28cf4d462d69e99d99a1601ae546e1c0e3116ca
Rolf Sievers <rolf.sievers@posteo.de>
2017-09-25 22:26:53 +0200

    That's it for today.

--- pixi.ts
+++ pixi.ts
@@ -28,7 +28,7 @@ class GridMap {
         return this.map.size;
     }
 
-    * [Symbol.iterator]() : IterableIterator<[GridPoint, Cell]> {
+    *[Symbol.iterator](): IterableIterator<[GridPoint, Cell]> {
         for (let [stringKey, value] of this.map) {
             let objectKey = JSON.parse(stringKey);
             yield [new GridPoint(objectKey.x, objectKey.y), value];
@@ -42,13 +42,13 @@ class GridPoint {
         return new PIXI.Point(1.5 * this.x, 0.866 * this.y);
     }
     // Iterates over all neighboring coordinates, starting to the top going clockwise.
-    * nbhd () {
-        yield new GridPoint(this.x, this.y-2);
-        yield new GridPoint(this.x + 1, this.y -1);
-        yield new GridPoint(this.x + 1, this.y +1);
-        yield new GridPoint(this.x, this.y+2);
-        yield new GridPoint(this.x - 1, this.y +1);
-        yield new GridPoint(this.x - 1, this.y -1);        
+    * nbhd() {
+        yield new GridPoint(this.x, this.y - 2);
+        yield new GridPoint(this.x + 1, this.y - 1);
+        yield new GridPoint(this.x + 1, this.y + 1);
+        yield new GridPoint(this.x, this.y + 2);
+        yield new GridPoint(this.x - 1, this.y + 1);
+        yield new GridPoint(this.x - 1, this.y - 1);
     }
 }
 
@@ -63,18 +63,18 @@ class Grid {
 
         this.makeCaptions();
     }
-    makeBoundingPoints() : {x : BoundingInterval, y: BoundingInterval} {
+    makeBoundingPoints(): { x: BoundingInterval, y: BoundingInterval } {
         let x = new BoundingInterval();
         let y = new BoundingInterval();
         for (var [point, cell] of this.content) {
             x.add(point.pixel.x);
             y.add(point.pixel.y);
         }
-        return {x, y};
+        return { x, y };
     }
     // This calculates the transformation which fits the level into a
     // given frame.
-    fitIntoFrame(width: number, height: number) : [number, PIXI.Point] {
+    fitIntoFrame(width: number, height: number): [number, PIXI.Point] {
         let bounds = this.makeBoundingPoints();
         let xPadding = 2;
         let yPadding = Math.sqrt(3);
@@ -83,16 +83,16 @@ class Grid {
         let yScale = height / (bounds.y.length + yPadding);
         let scale = Math.min(xScale, yScale);
         // offset
-        let xOffset = width / 2 - bounds.x.center*scale;
-        let yOffset = height / 2 - bounds.y.center*scale;
+        let xOffset = width / 2 - bounds.x.center * scale;
+        let yOffset = height / 2 - bounds.y.center * scale;
 
         return [scale, new PIXI.Point(xOffset, yOffset)];
     }
     // Precalculate all the captions and store them in the cells.
-    makeCaptions() : void {
+    makeCaptions(): void {
         // Right now I only support plain hints
         for (var [point, cell] of this.content) {
-            let count : number = 0;
+            let count: number = 0;
             for (let neighbor of point.nbhd()) {
                 let nbhdCell = this.content.get(neighbor);
                 if (nbhdCell != undefined) {
@@ -107,9 +107,9 @@ class Grid {
 }
 
 class BoundingInterval {
-    public min : number = null;
-    public max : number = null;
-    constructor() {}
+    public min: number = null;
+    public max: number = null;
+    constructor() { }
     add(value: number) {
         if (this.min == null || this.min > value) {
             this.min = value
@@ -118,18 +118,18 @@ class BoundingInterval {
             this.max = value
         }
     }
-    get length() : number {
+    get length(): number {
         return this.max - this.min;
     }
-    get center() : number {
+    get center(): number {
         return (this.min + this.max) / 2;
     }
 }
 
 class Cell {
-    caption : string; // Precomputed caption
-    constructor(public revealed: boolean, public mine: boolean) {}
-    get baseColor() : number {
+    caption: string; // Precomputed caption
+    constructor(public revealed: boolean, public mine: boolean) { }
+    get baseColor(): number {
         if (!this.revealed) {
             return 0xEEAA00
         } else {
@@ -140,12 +140,23 @@ class Cell {
             }
         }
     }
-    get hoverColor() : number {
+    get hoverColor(): number {
         if (!this.revealed) {
-            return 0xEEAAEE
+            return 0xEEAAAA
         } else {
             return this.baseColor
         }
+    }
+    get interactive(): boolean {
+        if (!this.revealed) {
+            return true
+        } else {
+            return false
+        }
+    }
+    get captionVisible(): boolean {
+        console.log("asked about revealed: ", this)
+        return this.revealed
     }
 }
 
@@ -163,7 +174,7 @@ function makeHexagon(radius: number = 1): PIXI.Polygon {
 
 
 // Setup for Pixi
-let app = new PIXI.Application(1000, 600, { backgroundColor: 0xffffff, antialias : true });
+let app = new PIXI.Application(1000, 600, { backgroundColor: 0xffffff, antialias: true });
 document.body.appendChild(app.view);
 
 app.renderer.view.style.border = "1px dashed black";
@@ -182,38 +193,46 @@ stage.y = offset.y;
 
 
 for (let [point, cell] of myGrid.content) {
-    let visibleHex = makeHexagon(0.9);
-    let interactiveHex = makeHexagon();
-    
-    let container = new PIXI.Container();
+    let container = new PIXI.Container()
 
-    // Add the hexagon to the stage
-    let hex = new PIXI.Graphics();
+    // Add the hexagon to the container
+    let hex = new PIXI.Graphics()
     container.addChild(hex)
-    
-    // visible part
-    hex.beginFill(0xFFFFFF);
-    hex.tint = cell.baseColor;
-    hex.drawPolygon(visibleHex);
+    hex.beginFill(0xFFFFFF)
+    hex.tint = cell.baseColor
+    hex.drawPolygon(makeHexagon(0.9))
 
+    // Add the text
     let virtualFontSize = 256
-    let text = new PIXI.Text(cell.caption, {fontFamily : 'Arial', fontSize: virtualFontSize, fill : 0x000000, align : 'center'});
+    let text = new PIXI.Text(cell.caption, { fontFamily: 'Arial', fontSize: virtualFontSize, fill: 0x000000, align: 'center' })
     text.anchor.x = 0.5
     text.anchor.y = 0.5
-    text.scale = new PIXI.Point(1/virtualFontSize, 1/virtualFontSize)
-    container.addChild(text);
+    text.scale = new PIXI.Point(1 / virtualFontSize, 1 / virtualFontSize)
+    container.addChild(text)
+    text.visible = cell.captionVisible
 
-    container.position = point.pixel;
+
+    // Position the container at the correct game location
+    container.position = point.pixel
+    app.stage.addChild(container)
 
     // interactive part
-    container.hitArea = visibleHex;
-    container.interactive = true;
-    container.on("mouseover", (event) => {
-        hex.tint = cell.hoverColor;
-    })
-    container.on("mouseout", (event) => {
-        hex.tint = cell.baseColor;
-    })
+    // TODO: With disabling eyecandy, this is more complicated. Some objects should be
+    // interactive but not show the button mode and have no hover effect.
+    if (cell.interactive) {
+        container.buttonMode = true
+        container.hitArea = makeHexagon(1)
+        container.interactive = true
+        container.on("mouseover", (event) => {
+            hex.tint = cell.hoverColor
+        })
+        container.on("mouseout", (event) => {
+            hex.tint = cell.baseColor
+        })
+        // TODO: I think that revealing the one hidden tile would be a good thing now
+        container.on("click", (event) => {
+            console.log(event)
+        })
+    }
 
-    app.stage.addChild(container);
 }
